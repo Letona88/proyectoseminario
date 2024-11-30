6adfc779b12004a7de85ddde8503029d296bfe9f
David Letona <davidletonajerez@gmail.com>
2024-11-30 01:46:00 -0600

    cambios en db

--- backend/db.js
+++ backend/db.js
@@ -2,10 +2,10 @@ const mysql = require('mysql2');
 
 // Configuración de conexión
 const db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
+    host: process.env.DB_HOST || '127.0.0.1', // Usa el valor del .env o un valor por defecto
+    user: process.env.DB_USER || 'root',
+    password: process.env.DB_PASSWORD || '',
+    database: process.env.DB_NAME || 'test_management',
 });
 
 // Exportar la conexión para usarla en otros archivos
